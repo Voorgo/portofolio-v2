23e95463e9669f514ecbb6304434f22bd8b1410c
Voorgo <serbancatalin10@yahoo.com>
2022-08-02 20:33:47 +0300

    Add smooth scroll

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,20 @@
 import "../styles/globals.css";
+import { useEffect } from "react";
+import Lenis from "@studio-freight/lenis";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    const lenis = new Lenis({
+      lerp: 0.1,
+      smooth: true,
+      direction: "vertical",
+    });
+    function raf() {
+      lenis.raf();
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+  }, []);
   return <Component {...pageProps} />;
 }
 
